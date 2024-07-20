77d5eb1724b7dd9df551293e725540ee29a400f0
Mark <mrmarckis@gmail.com>
2024-07-20 09:14:15 +0700

    demo zip

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def demo_zip():
+    values1 = list(range(2, 10))
+    values2 = list(range(10, 29, 3))
+    print(values1)
+    print(values2)
+    for v1, v2 in zip(values1, values2):
+        print("v1 =", v1, "v2 =", v2, "sum =", v1 + v2)
+
 def main():
     print("Hello main")
+    demo_zip()
 
 if __name__ == '__main__':
     main()
